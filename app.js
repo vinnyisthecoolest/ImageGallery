e6a98db1325c2a530133af369d3d6aece4c1da08
vinnyisthecoolest <vinnyisthecoolest@gmail.com>
2015-11-16 10:03:01 -0500

    Set up app for passport auth

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
-var express = require('express')
+var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
 var session = require('express-session');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var flash = require('connect-flash');
+
+// Mongo set up
+var configDB = require('./config/database.js');
+mongoose.connect(configDB.url);
+require('./config/passport')(passport);
 
 // Middleware
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({
+  secret: 'anystringoftext',
+  saveUninitialized: true,
+  resave: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
 app.set('view engine', 'ejs');
 
 // Routes
-require('./app/routes.js')(app);
+require('./app/routes.js')(app, passport);
 
 app.listen(port);
-console.log('Server is up at: ' + port);
+console.log('Server is up at port: ' + port);
